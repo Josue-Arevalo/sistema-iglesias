1ad09890f8885e9a3b37d991b6abb532f3efe34a
Josue Arevalo <josue.arevalo@idea.com.py>
2019-02-05 11:10:37 -0300

    Add codigo C# del sistema de iglesias

---
+++ Sistema-Iglesias/Common_Sistema/Entidades/Ciudad.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common_Sistema.Entidades
+{
+    class Ciudad
+    {
+        public int idCiudad { get; set; }
+        public string nombreCiudad { get; set; }
+    }
+}
---
+++ Sistema-Iglesias/Common_Sistema/Entidades/Iglesias.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common_Sistema.Entidades
+{
+    class Iglesias: Ciudad
+    {
+        public int idIglesia { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public int ciudad { get; set; }
+        public string pastorEncargado { get; set; }
+        public string nomeroTelefono { get; set; }
+
+    }
+}
